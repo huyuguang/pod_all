5d37b1ef197e48f81d3e0a8a30f58c5b01af7668
huyuguang <yuguang.hu@outlook.com>
2019-04-13 01:58:57 +0800

    Update basic_types_serialize.h

--- pod_core/basic_types_serialize.h
+++ pod_core/basic_types_serialize.h
@@ -24,6 +24,7 @@
 
 #include "basic_types.h"
 #include "ecc.h"
+#include "misc.h"
 
 inline constexpr size_t YasBinF() {
   return yas::options::binary | yas::options::ebig | yas::options::compacted;

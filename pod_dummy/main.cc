0977d9b5654e5211dcd1d5b8feb27dc1e64282dc
huyuguang <yuguang.hu@outlook.com>
2019-08-20 16:23:22 +0800

    update vcproj for msvc
    update vcproj for msvc
    
    update vcproj for msvc

--- pod_dummy/main.cc
+++ pod_dummy/main.cc
@@ -6,129 +6,218 @@
 #include "ecc.h"
 #include "ecc_pub.h"
 #include "misc.h"
+#include "public.h"
 #include "tick.h"
 #include "zkp.h"
-#include "public.h"
 
 #include "../pod_core/scheme_atomic_swap_serialize.h"
 #include "../pod_core/scheme_atomic_swap_vc_misc.h"
 
-bool InitAll(std::string const& data_dir) {
-  InitEcc();
+// return -m0^-1 % 2^64
+uint64_t test_inv(uint64_t m0) {
+  uint64_t inv = 1;
+  for (int i = 0; i < 63; ++i) {
+    inv = inv * inv;
+    inv = inv * m0;
+  }
+  assert(1 == (inv * m0));
+  inv = (-(int64_t)inv);
+
+  return inv;
+}
+
+uint32_t test_inv32(uint32_t m0) {
+  uint32_t inv = 1;
+  for (int i = 0; i < 63; ++i) {
+    inv = inv * inv;
+    inv = inv * m0;
+  }
+  assert(1 == (inv * m0));
+  inv = (-(int32_t)inv);
+  return inv;
+}
+
+mpz_class rand_mpz256() {
+  uint8_t s[32];
+  for (auto& i : s) {
+    i = rand() % 256;
+  }
+  auto str = misc::HexToStr(s, 32);
+  return mpz_class(str.data(), 16);
+}
+
+mpz_class rand_mpz512() {
+  uint8_t s[64];
+  for (auto& i : s) {
+    i = rand() % 256;
+  }
+  auto str = misc::HexToStr(s, 64);
+  return mpz_class(str.data(), 16);
+}
 
 #ifdef _DEBUG
-  InitZkp(false);
+const int kLoop = 1000;
 #else
-  InitZkp(true);
+const int kLoop = 1000000;
 #endif
 
-  auto ecc_pub_file = data_dir + "/" + "ecc_pub.bin";
+//std::array<mpz_class, kLoop> ga, gb, gc;
+//auto gp = mpz_class(
+//    "218882428718392752222464057452572750885483644004160343436982041865758084"
+//    "95617");
 
-  if (!OpenOrCreateEccPub(ecc_pub_file)) {
-    std::cerr << "Open or create ecc pub file " << ecc_pub_file << " failed\n";
-    return false;
+//void init_random() {
+//  Tick _tick_(__FUNCTION__);
+//  for (auto& i : ga) i = rand_mpz256();
+//  for (auto& i : gb) i = rand_mpz256();
+//}
+
+//void test_gmp_mul() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] * gb[i];    
+//  }
+//}
+//
+//void test_gmp_squared() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] * ga[i];    
+//  }
+//}
+//
+//void test_gmp_div() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] / gb[i];
+//  }
+//}
+//
+//void test_gmp_mod() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] % gp;
+//  }
+//}
+//
+//void test_gmp_mod512() {
+//  Tick _tick_(__FUNCTION__);
+//  auto aa = rand_mpz512();
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = aa % gp;
+//  }
+//}
+//
+//void test_gmp_mulmod() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] * gb[i];
+//    gc[i] = gc[i] % gp;
+//  }
+//}
+//
+//void test_gmp_add() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    //mpn_add_n(gc[i].get_mpz_t(), ga[i].get_mpz_t(), gb[i].get_mpz_t(), 4);
+//    gc[i] = ga[i] + gb[i];
+//  }
+//}
+//
+//void test_gmp_del() {
+//  Tick _tick_(__FUNCTION__);
+//  for (int i = 0; i < kLoop; ++i) {
+//    gc[i] = ga[i] - gb[i];
+//  }
+//}
+
+void test_mcl_fr_mul() {
+  std::vector<Fr> a(kLoop);
+  std::vector<Fr> b(kLoop);
+  std::vector<Fr> c(kLoop);
+  for (auto& i : a) i = FrRand();
+  for (auto& i : b) i = FrRand();
+  Fr d = FrZero();
+  Tick _tick_(__FUNCTION__);
+  for (int i = 0; i < kLoop; ++i) {
+    c[i] = a[i] * b[i];
+    d += c[i];
   }
-
-  std::string zkp_key_dir = data_dir + "/" + "zksnark_key";
-  if (zkp_key_dir.empty() || !fs::is_directory(zkp_key_dir)) {
-    std::cerr << "Open zkp_key_dir " << zkp_key_dir << " failed\n";
-    return false;
-  }
-
-  ZkpKey::instance(zkp_key_dir);
-
-  if (ZkpKey::instance().IsEmpty()) {
-    std::cerr << "Warning: zk key file not exist.\n";
-  }
-
-  return true;
+  std::cout << d << "\n";
 }
 
-bool VerifyZkProof(ZkProof const& proof, ZkVk const& vk,
-                   std::vector<uint64_t> const& public_o,
-                   std::vector<Fr> const& public_w, Fr seed_mimc3_digest,
-                   Fr ip_vw) {
-  using namespace libsnark;
-  auto const kCount = ZkpMimcCount();
-  assert(public_o.size() == kCount);
-  assert(public_w.size() == kCount);
+void test_ff_fr_mul_inv_sqrt() {
+  typedef libff::Fr<libsnark::default_r1cs_gg_ppzksnark_pp> ZkFr;
+  std::vector<ZkFr> a(kLoop);
+  std::vector<ZkFr> b(kLoop);
+  std::vector<ZkFr> c(kLoop);
+  for (auto& i : a) i = ZkFr::random_element();
+  for (auto& i : b) i = ZkFr::random_element();
 
-  // Create protoboard
-  protoboard<ZkFr> pb;
-
-  // Define variables
-  pb_variable<ZkFr> digest;
-  pb_variable<ZkFr> result;
-  pb_variable_array<ZkFr> o;
-  pb_variable_array<ZkFr> w;
-
-  // Allocate variables to protoboard
-  o.allocate(pb, kCount, "o");    // public
-  w.allocate(pb, kCount, "w");    // public
-  digest.allocate(pb, "digest");  // public
-  result.allocate(pb, "result");  // public
-
-  // This sets up the protoboard variables
-  // so that the first one (out) represents the public
-  // input and the rest is private input
-  pb.set_input_sizes(kCount * 2 + 2);
-
-  //// Add R1CS constraints to protoboard
-  // auto mimc3_const = ConvertToZkFr(Mimc3Const());
-  // auto mimcinv_const = ConvertToZkFr(MimcInvConst());
-  // AtomicSwapVcGadget<ZkFr> g(pb, mimc3_const, mimcinv_const, seed,
-  // seed_rand,
-  //                           digest, result, o, w);
-  // g.generate_r1cs_constraints();
-
-  // public statement
-  for (size_t i = 0; i < kCount; ++i) {
-    pb.val(o[i]) = public_o[i];
-    pb.val(w[i]) = ConvertToZkFr(public_w[i]);
+  ZkFr d;
+  {
+    Tick _tick_("ff add");
+    for (int i = 0; i < kLoop; ++i) {
+      d += a[i];
+    }
   }
-  pb.val(digest) = ConvertToZkFr(seed_mimc3_digest);
-  pb.val(result) = ConvertToZkFr(ip_vw);
+  {
+    Tick _tick_("ff del");
+    for (int i = 0; i < kLoop; ++i) {
+      d -= a[i];
+    }
+  }
 
-  // Verify proof
-  bool verified =
-      r1cs_gg_ppzksnark_verifier_strong_IC<default_r1cs_gg_ppzksnark_pp>(
-          vk, pb.primary_input(), proof);
-  assert(verified);
-  return verified;
+  {
+    Tick _tick_("ff mul");
+    for (int i = 0; i < kLoop; ++i) {
+      c[i] = a[i] * b[i];
+    }
+  }
+
+  {
+    Tick _tick_("ff inv");
+    for (auto& i : a) i = i.inverse();
+  }
+
+  {
+    Tick _tick_("ff power");
+    for (int i = 0; i < kLoop/1000; ++i) {
+      c[i] = a[i] ^ c[i].as_bigint();
+    }
+  }
+
+  {
+    Tick _tick_("ff squared");
+    for (int i = 0; i < kLoop; ++i) {
+      a[i] = a[i].squared();
+    }
+  }
+  {
+    Tick _tick_("ff sqrt");
+    for (int i = 0; i < kLoop/1000; ++i) {
+      a[i] = a[i].sqrt();
+    }
+  }
+
+  std::cout << d << "\n";
 }
 
-void CheckProof(std::string const& file) {
-  ZkProof proof;
-  std::vector<uint64_t> public_offset;
-  std::vector<Fr> public_w;
-  Fr seed_mimc3_digest;
-  Fr ip_vw;
-
-  yas::file_istream is(file.c_str());
-  yas::binary_iarchive<yas::file_istream, YasBinF()> ia(is);
-  ia.serialize(proof);
-  ia.serialize(public_offset);
-  ia.serialize(public_w);
-  ia.serialize(seed_mimc3_digest);
-  ia.serialize(ip_vw);
-
-  auto const& vk = *ZkpKey::instance().GetZkVk("atomic_swap_vc");
-
-  bool ret = VerifyZkProof(proof, vk, public_offset, public_w,
-                           seed_mimc3_digest, ip_vw);
-  assert(ret);
-  std::cout << "VerifyZkProof: " << file << " " << (ret ? "true" : "false")
-            << "\n";
-}
 
 int main(int /*argc*/, char** /*argv*/) {
-  InitAll(".");
+  InitEcc();
+  libsnark::default_r1cs_gg_ppzksnark_pp::init_public_params();
 
-  auto range = boost::make_iterator_range(fs::directory_iterator("./temp"), {});
-  for (auto& entry : range) {
-    auto fullpath = entry.path().string();
-    CheckProof(fullpath);
-  }
-
+  //init_random();
+  //test_gmp_mul();
+  //test_gmp_squared();
+  //test_gmp_add();
+  //test_gmp_del();
+  //test_gmp_div();
+  //test_gmp_mod();
+  //test_gmp_mod512();
+  //test_gmp_mulmod();
+  test_mcl_fr_mul();
+  test_ff_fr_mul_inv_sqrt();
   return 0;
 }
